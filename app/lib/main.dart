16191002fee1db2a0bddae5ff7fd5d49868e446e
natopasnato <puropurokanakana346@gmail.com>
2025-10-21 02:13:51 +0000

    ある程度準備完了

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: 'https://eeebswpwdksumptowcpr.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZWJzd3B3ZGtzdW1wdG93Y3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4ODMzNzMsImV4cCI6MjA3NjQ1OTM3M30.siN_fQEEm3-JaX8JDvY51eIPZcjjGxUfq_Gq6__Jk7w',
+  );
+  
   runApp(const MyApp());
 }
 
